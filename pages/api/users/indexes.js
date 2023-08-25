bba98b40ac6f2c97bcaf6b35f90556113470061c
Gihan Shirantha <shirantha1999@gmail.com>
2023-08-25 23:07:10 +0530

    api create

---
+++ pages/api/users/indexes.js
@@ -0,0 +1,29 @@
+import connectMongo from '../../../Database/Connection';
+
+export default async function handler(req, res) {
+  try {
+    await connectMongo();
+  } catch (error) {
+    return res.status(405).json({ error: "Error in the connection" });
+  }
+  
+  const { method } = req;
+  switch (method) {
+    case 'GET':
+      res.status(200).json({ method, name: 'GET Request' });
+      break;
+    case 'POST':
+      res.status(200).json({ method, name: 'POST Request' });
+      break;
+    case 'PUT':
+      res.status(200).json({ method, name: 'PUT Request' });
+      break;
+    case 'DELETE':
+      res.status(200).json({ method, name: 'DELETE Request' });
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'POST', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
