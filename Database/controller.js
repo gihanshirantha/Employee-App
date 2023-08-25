ade9f4000bef768993a85890869f5f1c60b9bb27
Gihan Shirantha <shirantha1999@gmail.com>
2023-08-25 23:57:06 +0530

    Get User request

--- Database/controller.js
+++ Database/controller.js
@@ -1,11 +1,18 @@
 // Controller
+import User from "./Model/user"
 
+
+// get User
 export async function getUsers(req,res){
     try {
-        res.status(200).json({
-            user:"GET request"
-        })
+        const user=await User.find({})
+
+        if(!user) return res.status(404).json({error:"Data Not found"})
+
+        res.status(200).json(user)
     } catch (error) {
         res.status(404).json({error:"Error while Fetching Data"})
     }
 }
+
+// Post user
